00e2e927ce4f999f8ed523ae434bd3ffc0ff5065
ogiwara <ogiwara.dev@gmail.com>
2018-12-09 22:41:56 +0900

    Part 4

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,49 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Penmark",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Penmark"),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords()
     );
   }
 
 }
 
 class RandomWordsState extends State<RandomWords>{
+
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions(){
+     return ListView.builder(
+         padding: const EdgeInsets.all(16.0),
+         itemBuilder: (_, i){
+           if(i.isOdd) return  Divider();
+
+           final index = i ~/ 2;
+           if (index >= _suggestions.length) {
+             _suggestions.addAll(generateWordPairs().take(10));
+           }
+           return _buildRow(_suggestions[index]);
+         }
+     );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold (
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
 
