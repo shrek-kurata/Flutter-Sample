af7531248fe10256434f09da541b4854594c1f7f
ogiwara <ogiwara.dev@gmail.com>
2018-12-09 22:33:02 +0900

    Part 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,10 +13,25 @@ class MyApp extends StatelessWidget{
           title: Text("Penmark"),
         ),
         body: Center(
-          child: Text("Hello, World"),
+          child: RandomWords(),
         ),
       ),
     );
   }
 
 }
+
+class RandomWordsState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => new RandomWordsState();
+
+}
+
