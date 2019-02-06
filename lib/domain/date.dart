fcfa377d1389938a22b28ca4d9aed5cb43e63d62
ogiwara <ogiwara.dev@gmail.com>
2019-02-06 21:09:57 +0900

    update Date equality operator

--- lib/domain/date.dart
+++ lib/domain/date.dart
@@ -1,4 +1,5 @@
 import 'package:meta/meta.dart';
+import 'package:quiver/core.dart';
 
 @immutable
 class Date{
@@ -10,4 +11,18 @@ class Date{
     @required this.year,
     @required this.month,
     @required this.day });
+
+  @override
+  bool operator ==(Object other) {
+    if(other is! Date){
+      return false;
+    }
+    final that = (other as Date);
+    return (year == that.year && month == that.month && day == that.day);
+  }
+
+  @override
+  int get hashCode {
+    return hash3(year, month, day);
+  }
 }
