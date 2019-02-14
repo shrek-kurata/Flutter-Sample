8465f1624727ab784a161820113371c3f09b820e
ogiwara <ogiwara.dev@gmail.com>
2019-02-14 20:41:53 +0900

    update sample translator

--- lib/infrastracture/dto/CampusTranslator.dart
+++ lib/infrastracture/dto/CampusTranslator.dart
@@ -1,25 +1,26 @@
 import 'package:penmark/domain/campus.dart';
+import 'package:tuple/tuple.dart';
 
 class CampusTranslator{
 
-  final map = [
-    [Campus.Mita, "三田"],
-    [Campus.Hiyoshi, "日吉"],
-    [Campus.Shounan, "湘南藤沢"],
-    [Campus.Yagami, "矢上"],
-    [Campus.Shinanomachi, "信濃町"],
-    [Campus.Shiba, "芝共立"]
+  final List<Tuple2<Campus, String>> map = [
+    Tuple2(Campus.Mita, "三田"),
+    Tuple2(Campus.Hiyoshi, "日吉"),
+    Tuple2(Campus.Shounan, "湘南藤沢"),
+    Tuple2(Campus.Yagami, "矢上"),
+    Tuple2(Campus.Shinanomachi, "信濃町"),
+    Tuple2(Campus.Shiba, "芝共立"),
   ];
 
   Campus fromPersistence(String str){
-    final index = map.indexWhere((e) => e[1] == str);
+    final index = map.indexWhere((e) => e.item2 == str);
     assert(index != -1);
-    return map[index][0] as Campus;
+    return map[index].item1;
   }
 
   String toPersistence(Campus campus){
-    final index = map.indexWhere((e) => e[0] == campus);
+    final index = map.indexWhere((e) => e.item1 == campus);
     assert(index != -1);
-    return map[index][1] as String;
+    return map[index].item2;
   }
 }
